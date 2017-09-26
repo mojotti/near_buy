d364675fd61ad1a46c3b779f9d5dcfc242ac28c7
Risto-Matti Piirainen <ristomatti91@hotmail.com>
2017-09-26 18:08:27 +0300

    renaming stuff

--- react-native/pages/HomeScreen.js
+++ react-native/pages/HomeScreen.js
@@ -1,16 +1,20 @@
 'use strict';
 
 import React from 'react';
-import { Image, Platform , StyleSheet, Text, View } from 'react-native';
+import { Image, Platform , StyleSheet,
+        Text, View, ScrollView, Button } from 'react-native';
+import { logout } from '../redux/actions/auth';
+import { connect } from 'react-redux';
 
 const LOCALHOST = (Platform.OS === 'ios') ? 'localhost' : '10.0.2.2';
 const base64  = require('base-64');
 
 
-export default class HomeScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Home',
-  };
+class HomeScreen extends React.Component {
+  userLogout(e) {
+      this.props.onLogout();
+      e.preventDefault();
+  }
 
   state = {
         data: ''
@@ -48,11 +52,15 @@ export default class HomeScreen extends React.Component {
   render() {
     return (
       <View style={[styles.container]}>
-        <Image
-          resizeMode="contain"
-          style={styles.logo}
-          source={require('./logo.png')}
-        />
+        <View style={{padding: 20}}>
+          <Text style={{fontSize: 27}}>
+              {`Welcome ${this.props.username}`}
+          </Text>
+          <View style={{margin: 20}}/>
+        </View>
+        <Text style={[styles.header]}>
+          Your items:
+        </Text>
         <Text>
            {"Title: " + this.state.data.title}
         </Text>
@@ -62,6 +70,11 @@ export default class HomeScreen extends React.Component {
          <Text>
             {"Price: " + this.state.data.price + " €"}
          </Text>
+         <Button
+            onPress={(e) => this.userLogout(e)}
+            title="Logout"
+            style = {{margin: 15}}
+          />
       </View>
     );
     }
@@ -74,8 +87,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  logo: {
-    width: 350,
-    height: 150
-  }
+  header: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginBottom: 20
+  },
 });
+
+const mapStateToProps = (state, ownProps) => {
+    return {
+        username: state.auth.username
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onLogout: () => { dispatch(logout()); }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
---
+++ react-native/pages/Secured.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { ScrollView, Text, View, Button } from 'react-native';
+import HomeScreen from './HomeScreen.js'
+
+
+class Secured extends Component {
+    render() {
+      return (
+        <HomeScreen />
+      );
+    }
+}
+
+
+export default Secured;
