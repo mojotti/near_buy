e48bb26e9433f12582700fc03a785d61a2647d0c
Risto-Matti Piirainen <ristomatti.piirainen@gmail.com>
2018-02-27 18:39:23 +0200

    application.js prettify

--- react-native/components/Application.js
+++ react-native/components/Application.js
@@ -4,19 +4,19 @@ import Login from './Login';
 import Secured from './Secured';
 
 class Application extends React.Component {
-    render() {
-        if (this.props.isLoggedIn) {
-            return <Secured />;
-        } else {
-            return <Login />;
-        }
+  render() {
+    if (this.props.isLoggedIn) {
+      return <Secured />;
+    } else {
+      return <Login />;
     }
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-    return {
-        isLoggedIn: state.auth.isLoggedIn,
-    };
+  return {
+    isLoggedIn: state.auth.isLoggedIn,
+  };
 };
 
 export default connect(mapStateToProps)(Application);
