4ad95b28f51ccd235b93bc0f7bb9f7ccded930d5
Risto-Matti Piirainen <ristomatti.piirainen@gmail.com>
2018-03-30 11:05:15 +0300

    add redux-persist

--- react-native/App.js
+++ react-native/App.js
@@ -2,14 +2,17 @@
 
 import React from 'react';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import Application from './src/components/Application';
-import store from './src/redux/ReduxStore';
+import { store, persistor } from './src/redux/ReduxStore';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Application />
+        <PersistGate loading={null} persistor={persistor}>
+          <Application />
+        </PersistGate>
       </Provider>
     );
   }
