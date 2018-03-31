76290faf00b6c6661646aea48b0983938e2915a1
Risto-Matti Piirainen <ristomatti.piirainen@gmail.com>
2018-03-31 10:49:34 +0300

    add encryption and whitelist to persisted data

--- react-native/src/redux/ReduxStore.js
+++ react-native/src/redux/ReduxStore.js
@@ -1,15 +1,22 @@
 import { createStore } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
+import createEncryptor from 'redux-persist-transform-encrypt';
+
 import storage from 'redux-persist/lib/storage';
 import rootReducer from './reducers/RootReducer';
+
+const encryptor = createEncryptor({
+  secretKey: '$b1!j)%z_$n&adltvihhoesu24+^b!89c@emh83w1kc%7yo$ke',
+});
 
 const persistConfig = {
   key: 'root',
   storage,
+  whitelist: ['authorizationReducer'], // store only auth info
+  transforms: [encryptor],
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const reducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer);
+export const store = createStore(reducer);
 export const persistor = persistStore(store);
-
